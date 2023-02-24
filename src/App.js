99b0065ff2ce40f8fbfc900f6166411c9203b065
mohamad hosein <mmdtrni@gmail.com>
2023-02-24 08:52:09 +0330

    added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
-import {BrowserRouter as Router , Routes, Route, Link} from 'react-router-dom';
+import {BrowserRouter as Router , Routes, Route, Link, } from 'react-router-dom';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
 
 import { HomeComponent } from './components/home';
 import { RandomTaskComponent } from './components/randomTask';
+
 
 function App() {
     const client = new QueryClient({defaultOptions: {
@@ -16,6 +17,7 @@ function App() {
         <>
             <QueryClientProvider client={client}>
                 <Router>
+                
                     <div className='navbar'>
                         <Link to={"/"} > <button className='btn'>Home</button></Link>
                         <Link to={"/random-task"}><button className='btn'>Random Task</button></Link>
--- src/components/randomTask.js
+++ src/components/randomTask.js
@@ -1,19 +1,47 @@
 import React from 'react';
 import axios from 'axios';
 import { useQuery } from '@tanstack/react-query'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
+import {Task} from './task';
+
 export const RandomTaskComponent = () => {
-    const [todoList, setTodoList] = useState([]);
-    const { isLoading } = useQuery([], () => {
+    const [randomTodoList, setRandomTodoList] = useState([]);
+    const { isLoading, isFetched, isSuccess } = useQuery(["task"], () => {
         return axios.get('https://jsonplaceholder.typicode.com/todos')
-        .then((res) => {
-            setTodoList(res.data[Math.floor(Math.random() * res.data.length)].title)
-        })
+        .then(res => setRandomTodoList(res.data))
     })
+    
+    const deleteTaskHandler = (id) => {
+        setRandomTodoList(randomTodoList.filter((task) => {
+            return task.id !== id;
+        }));
+    };
+
+    const completeTask = (id) => {
+        setRandomTodoList(randomTodoList.map((task) => {
+            if (task.id === id) {
+                return {...task, completed: true};
+            } else {
+                return task;
+            }
+        }))
+    };
 
     if (isLoading) return <h1>Loading..</h1>
 
-    return <h1>{todoList}</h1>
-
+    return (
+        <div className="list">
+            {randomTodoList.map((task) => {
+                    return <Task
+                        taskName={task.title}
+                        id={task.id}
+                        completed={task.completed}
+                        deleteTaskHandler={deleteTaskHandler}
+                        completeTask={completeTask}
+                    />;
+                }
+            )}
+        </div>
+    )
     
 }
