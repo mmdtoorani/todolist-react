0d070879d1432fa1f8eb649e360f07c7aa2a93df
mohamad hosein <mmdtrni@gmail.com>
2023-02-24 05:50:27 +0330

    routing

--- src/App.js
+++ src/App.js
@@ -1,59 +1,25 @@
 import './App.css';
-import {useState} from 'react'
-import {Task} from './components/task'
+import {useState, useEffect} from 'react';
+import axios from 'axios';
+import {BrowserRouter as Router , Routes, Route, Link} from 'react-router-dom';
+
+import { HomeComponent } from './components/home';
+import { RandomTaskComponent } from './components/randomTask';
 
 function App() {
-    const [todoList, setTodoList] = useState([]);
-    const [newTask, setNewTask] = useState("");
-
-    const changeHandler = (event) => {
-        setNewTask(event.target.value);
-    };
-
-    const newTaskHandler = (event) => {
-        const task = {
-            id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
-            taskName: newTask,
-            completed: false
-        }
-        return setTodoList([...todoList, task])
-    };
-
-    const deleteTaskHandler = (id) => {
-        setTodoList(todoList.filter((task) => {
-            return task.id !== id;
-        }));
-    };
-
-    const completeTask = (id) => {
-        setTodoList(todoList.map((task) => {
-            if (task.id === id) {
-                return {...task, completed: true};
-            } else {
-                return task;
-            }
-        }))
-    };
+    
 
     return (
-        <div>
-            <div className="add-task">
-                <input type="text" onChange={changeHandler}></input>
-                <button onClick={newTaskHandler}>Add task</button>
-            </div>
-            <div className="list">
-                {todoList.map((task) => {
-                        return <Task
-                            taskName={task.taskName}
-                            id={task.id}
-                            completed={task.completed}
-                            deleteTaskHandler={deleteTaskHandler}
-                            completeTask={completeTask}
-                        />;
-                    }
-                )}
-            </div>
-        </div>
+        <>
+            <Router>
+                <Link to={"/"}>Home</Link>
+                <Link to={"/random-task"}>Random Task</Link>
+                <Routes>
+                    <Route path={"/"} element={<HomeComponent />} />
+                    <Route path={"/random-task"} element={<RandomTaskComponent />} />
+                </Routes>
+            </Router>
+        </>
     )
 }
 
---
+++ src/components/home.js
@@ -0,0 +1,60 @@
+import React from "react";
+import {useState, useEffect} from 'react';
+import {Task} from './task';
+
+    
+export const HomeComponent = () => {
+
+    const [todoList, setTodoList] = useState([]);
+    const [newTask, setNewTask] = useState("");
+
+    const changeHandler = (event) => {
+        setNewTask(event.target.value);
+    };
+
+    const newTaskHandler = (event) => {
+        const task = {
+            id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
+            taskName: newTask,
+            completed: false
+        }
+        return setTodoList([...todoList, task])
+    };
+
+    const deleteTaskHandler = (id) => {
+        setTodoList(todoList.filter((task) => {
+            return task.id !== id;
+        }));
+    };
+
+    const completeTask = (id) => {
+        setTodoList(todoList.map((task) => {
+            if (task.id === id) {
+                return {...task, completed: true};
+            } else {
+                return task;
+            }
+        }))
+    };
+
+    return (
+        <div>
+            <div className="add-task">
+                <input type="text" onChange={changeHandler}></input>
+                <button onClick={newTaskHandler}>Add task</button>
+            </div>
+            <div className="list">
+                {todoList.map((task) => {
+                        return <Task
+                            taskName={task.taskName}
+                            id={task.id}
+                            completed={task.completed}
+                            deleteTaskHandler={deleteTaskHandler}
+                            completeTask={completeTask}
+                        />;
+                    }
+                )}
+            </div>
+        </div>
+    )
+}
